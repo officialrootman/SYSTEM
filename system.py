8e4e01a6829715fd83ccf9e295e1fbcb4aa0f08f
officialrootman <muhammetcanaltan41@gmail.com>
2025-03-16 12:03:06 +0300

    Update system.py

--- system.py
+++ system.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, world! Welcome to my website."
+    return "Sistem Açıldı!"
 
 if __name__ == '__main__':
     app.run(debug=True)
